989cb5e5078399c7faeef8a67c76efbc17848b4e
Daniel Faria <daniel.faria@phila.gov>
2020-06-23 00:08:42 -0400

    added favicon and some meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,26 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Source List: Italian',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: 'A source list of Italian to help you learn italian' },
+      {
+        hid: 'no-cache-1',
+        "http-equiv": "Pragma",
+        content: "no-cache",
+      },
+      {
+        hid: 'no-cache-2',
+        "http-equiv": "Expires",
+        content: "0",
+      },
+      {
+        hid: 'no-cache-3',
+        "http-equiv": "Cache-Control",
+        content: "no-cache, no-store, must-revalidate",
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
